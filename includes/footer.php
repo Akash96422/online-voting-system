5d23bdc471f1cccce1838ec396656a29048553ab
Akash96422 <85032788+Akash96422@users.noreply.github.com>
2022-07-14 22:58:42 +0530

    First Commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> &copy; 2022 Brought To You By<a href="https://github.com/Akash96422"> Akash sharma</a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
